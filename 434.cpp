2556104a0f2f4c4034683444b2252887f1fab2fa
misternaimur <jawadnaimur@gmail.com>
2025-11-02 21:11:11 +0600

    solve 467

---
+++ 434.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int t ;
+    cin >> t ;
+    for ( int i ; i < t ; i++)
+    {
+        int x,y,z;
+        cin >> x >> y >> z;
+        if ( x > y && x > z){
+            cout << "Setter" << endl;
+        }
+        else if (y > x && y >> z ){
+            cout << "Tester" << endl;
+        }
+        else if (z > x && z > y ){
+            cout << "Editorialist" << endl;
+        }
+        
+        
+        
+    }
+}
